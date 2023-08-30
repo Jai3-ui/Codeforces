3ca91be7b911a609c9b454b5ffba411f7a7cbbc3
Jai <jai20070@iiitd.ac.in>
2023-08-30 10:40:34 +0530

    Question 2

---
+++ Pupil/Question 2/Q2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits\stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    while(n--){
+        string str;
+        cin>>str;
+    }
+}
+
+int main(){
+    solve();
+    return 0;
+}
