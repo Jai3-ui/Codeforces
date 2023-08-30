49d1e754b968c0b4f9d77b865971bd73bd2fc64a
Jai <jai20070@iiitd.ac.in>
2023-08-30 12:01:37 +0530

    Question 2 Solution

--- Pupil/Question 2/Q2.cpp
+++ Pupil/Question 2/Q2.cpp
@@ -3,11 +3,24 @@
 using namespace std;
 
 void solve(){
+
     int n;
     cin>>n;
+    unordered_map<string,int> mp;
     while(n--){
         string str;
         cin>>str;
+        if(mp.find(str)==mp.end()){
+            mp[str]=1;
+            cout<<"OK"<<endl;
+        }
+        else{
+            string val=to_string(mp[str]);
+            cout<<str<<""<<val<<endl;
+            mp[str]++;
+
+        }
+
     }
 }
 
